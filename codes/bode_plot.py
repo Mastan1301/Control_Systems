8972aaccee5045c9fbb885a6fe3756be04e9e3cb
Shaik_Mastan <shaikmastadino@gmail.com>
2020-04-23 19:00:30 +0530

    Correct the asymptoticPlot() function

--- codes/bode_plot.py
+++ codes/bode_plot.py
@@ -7,35 +7,34 @@ import subprocess
 import shlex
 #end if
 
-def CalculatePolesAndZeros(num, den):
+def CalculatePolesAndZeros(num, den): #returns an array of zeros and poles
 	pz = []
 	pz.append(np.flip(np.roots(num)))
 	pz.append(np.flip(np.roots(den)))
 	return pz
 
-def asymptoticPlot(x, pz):
+def asymptoticPlot(x, pz): # returns the values for asymptotic Bode plot
     arr = np.zeros((len(x)))
     arr[0] = 20*np.log10(1e6) # poles at origin
     val = arr[0]
     slope = 0
     j, k = 0, 0
     for i in range(1, len(x)):
-        while j < len(pz[0]) and x[i] == abs(pz[0][j]): # check for multiple poles/ zeros
+        while j < len(pz[0]) and x[i-1] == abs(pz[0][j]): # check for multiple poles/ zeros
             slope += 20
             j += 1
-        while k < len(pz[1]) and x[i] == abs(pz[1][k]):
+        while k < len(pz[1]) and x[i-1] == abs(pz[1][k]):
             slope -= 20
             k += 1
 
-        temp1 = x[i-1]
-        temp2 = x[i]
-        if temp1 == 0.0:
-            temp1 = 1e-2
-        if temp2 == 0.0:
-            temp2 = 1e-2
+        if x[i-1] == 0.0: # avoiding division by zero
+            x[i-1] = 1e-2
+        if x[i] == 0.0:
+            x[i] = 1e-2
 
-        print(temp1, temp2)
-        val += slope*np.log10(temp2/temp1)
+        if(x[i] != x[i-1]): 
+            val += slope*np.log10(x[i]/x[i-1])
+
         arr[i] = val
 
     return arr
@@ -54,23 +53,24 @@ for i in range(len(pz)):
     for j in range(len(pz[i])):
         x.append(abs(pz[i][j]))
 
-x.sort()
+x.append(1e4)
+x.sort() # w-axis for drawing the theoretical asymptotic plot
 y = asymptoticPlot(x, pz)
-print(y)
 
 plt.figure()
 plt.xlabel("f")
 plt.ylabel("H(f)")
 plt.title("Bode Plot")
-plt.semilogx(w, mag)    # Bode magnitude plot'''
+plt.semilogx(w, mag)    # Using in-built function
 
-plt.plot(x, y)
+plt.semilogx(x, y) # Theoretical plot
 plt.legend(["Using in-built function" , "Asymptotic Plot"])
-plt.grid() 
+plt.grid()
 plt.show()
+
 ''' if using termux
 plt.savefig('./figs/ee18btech11001/ee18btech11001_2.pdf')
 plt.savefig('./figs/ee18btech11001/ee18btech11001_2.eps')
-subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_2.pdf"))
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_2.pdf"))'''
 #else
-#plt.show() '''
+#plt.show() 
