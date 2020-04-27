714e9390adcfd0ce4f51134d9074f03341745f91
Shaik_Mastan <shaikmastadino@gmail.com>
2020-04-27 12:38:33 +0530

    Update the repo

--- codes/ee18btech11039.py
+++ codes/ee18btech11039.py
@@ -25,7 +25,7 @@ plt.subplot(2, 1, 1)
 plt.xlabel("$\omega$")
 plt.ylabel("20$log_{10}(|G(j\omega)|$")
 plt.title("Magnitude Plot")
-plt.semilogx(w, mag) # Using in-built function 
+plt.semilogx(w, mag) 
 plt.plot(wg, gm, 'o')
 plt.plot(wp, 0, 'o')
 plt.text(wg+0.5, gm+2, '({}, {})'.format(round(wg, 2), round(gm, 2)))
@@ -37,18 +37,16 @@ plt.subplot(2, 1, 2)
 plt.xlabel("$\omega$")
 plt.ylabel("$\phi(j\omega)$")
 plt.title("Phase Plot")
-plt.semilogx(w, phase)   # Using in-built function
+plt.semilogx(w, phase)  
 plt.plot(wp, pm+180, 'o')
 plt.plot(wg, 180, 'o')
 plt.text(wp+0.5, pm+178, '({}, {})'.format(round(wp, 2), round(pm+180, 2)))
 plt.text(wg+0.5, 178, '({}, {})'.format(round(wg, 2), 180))
 plt.grid()
 
-plt.show()
-
-''' #if using termux
+#if using termux
 plt.savefig('./figs/ee18btech11039/ee18btech11039.pdf')
 plt.savefig('./figs/ee18btech11039/ee18btech11039.eps')
-subprocess.run(shlex.split("termux-open ./figs/ee18btech110039/ee18btech11039.pdf"))'''
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11039/ee18btech11039.pdf"))
 #else
 #plt.show() 
